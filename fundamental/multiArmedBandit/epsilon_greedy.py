4e125c976106e018fc1b6cca51c56699f730ce7a
nihilKnight <takigawa_yoshino@163.com>
2025-07-31 09:43:10 +0800

    added init_prob explanation.

--- fundamental/multiArmedBandit/epsilon_greedy.py
+++ fundamental/multiArmedBandit/epsilon_greedy.py
@@ -4,10 +4,13 @@ from solver import Solver
 
 class EpsilonGreedy(Solver):
     """ epsilon贪婪算法,继承Solver类 """
+    # 乐观初始化法（Optimistic Initial Values）加速收敛。
+    # 将初始奖励期望估值设定为1.0，使每个拉杆看起来都很“好”，算法会主动多试几次，起到增加探索的作用。
+    # 在 epsilon 较小或完全贪婪时有作用，因为它避免了“第一次尝试差臂后就永远不碰”的问题。
     def __init__(self, bandit, epsilon=0.01, init_prob=1.0):
         super(EpsilonGreedy, self).__init__(bandit)
         self.epsilon = epsilon
-        #初始化拉动所有拉杆的期望奖励估值
+        # 初始化拉动所有拉杆的期望奖励估值
         self.estimates = np.array([init_prob] * self.bandit.K)
 
     def run_one_step(self):
@@ -16,7 +19,9 @@ class EpsilonGreedy(Solver):
         else:
             k = np.argmax(self.estimates)  # 选择期望奖励估值最大的拉杆
         r = self.bandit.step(k)  # 得到本次动作的奖励
+        print(self.estimates, k, r, end='\t\t')
         self.estimates[k] += 1. / (self.counts[k] + 1) * (r - self.estimates[k])
+        print(self.estimates[k])
         return k
 
 
