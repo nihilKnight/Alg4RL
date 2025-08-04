d181b92ba7695ec055c8da6c31ddff8714953fe5
nihilKnight <takigawa_yoshino@163.com>
2025-08-04 09:36:39 +0800

    added markov decision process chapter.

---
+++ fundamental/markovDecisionProcess/compute_value.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def compute(P, rewards, gamma, states_num):
+    ''' 利用贝尔曼方程的矩阵形式计算解析解,states_num是MRP的状态数 '''
+    rewards = np.array(rewards).reshape((-1, 1))  #将rewards写成列向量形式
+    value = np.dot(np.linalg.inv(np.eye(states_num, states_num) - gamma * P),
+                   rewards)
+    return value
+
+
+gamma = 0.5
+# 转化后的MRP的状态转移矩阵
+P_from_mdp_to_mrp = [
+    [0.5, 0.5, 0.0, 0.0, 0.0],
+    [0.5, 0.0, 0.5, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.5, 0.5],
+    [0.0, 0.1, 0.2, 0.2, 0.5],
+    [0.0, 0.0, 0.0, 0.0, 1.0],
+]
+P_from_mdp_to_mrp = np.array(P_from_mdp_to_mrp)
+R_from_mdp_to_mrp = [-0.5, -1.5, -1.0, 5.5, 0]
+
+V = compute(P_from_mdp_to_mrp, R_from_mdp_to_mrp, gamma, 5)
+print("MDP中每个状态价值分别为\n", V)
